f96124e285ce54b6ae6e1fd30e122ec11140b99d
S-Marnat <242020513+S-Marnat@users.noreply.github.com>
2026-05-25 17:13:01 +0200

    feat: implémentation des DTO

--- P7CreateRestApi/Models/CurvePointCreateDTO.cs
+++ P7CreateRestApi/Models/CurvePointCreateDTO.cs
@@ -1,12 +1,21 @@
-﻿namespace P7CreateRestApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P7CreateRestApi.Models
 {
     public class CurvePointCreateDTO
     {
-        public int Id { get; set; }
-        public byte? CurveId { get; set; }
-        public DateTime? AsOfDate { get; set; }
-        public double? Term { get; set; }
-        public double? CurvePointValue { get; set; }
-        public DateTime? CreationDate { get; set; }
+        [Required(ErrorMessage = "L'identifiant de la courbe à laquelle appartient ce point doit être renseigné")]
+        public byte CurveId { get; set; }
+
+        [Required(ErrorMessage = "La date à laquelle la valeur du point est valable doit être renseignée")]
+        public DateTime AsOfDate { get; set; }
+
+        [Required(ErrorMessage = "Le nombre d'années doit être renseigné")]
+        [Range(0, double.MaxValue, ErrorMessage = "Le nombre d'années renseigné doit être positive")]
+        public double Term { get; set; }
+
+        [Required(ErrorMessage = "La valeur du point doit être renseignée")]
+        [Range(0, double.MaxValue, ErrorMessage = "La valeur du point renseignée doit être positive")]
+        public double CurvePointValue { get; set; }
     }
 }
