437c595614b0e50ab00a311035f85d1d6a328fda
S-Marnat <242020513+S-Marnat@users.noreply.github.com>
2026-05-26 14:20:50 +0200

    feat: implémentation des DTO

--- P7CreateRestApi/Models/RatingUpdateDTO.cs
+++ P7CreateRestApi/Models/RatingUpdateDTO.cs
@@ -1,11 +1,27 @@
-﻿namespace P7CreateRestApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P7CreateRestApi.Models
 {
     public class RatingUpdateDTO
     {
-        public int Id { get; set; }
+        [Required(ErrorMessage = "La note donnée par Moody’s doit être renseignée")]
+        [MinLength(1, ErrorMessage = "La note donnée par Moody’s doit contenir au moins 1 caractère")]
+        [MaxLength(4, ErrorMessage = "La note donnée par Moody’s ne doit pas dépasser 4 caractères")]
+        [RegularExpression(@"^(Aaa|Aa[1-3]|A[1-3]|Baa[1-3]|Ba[1-3]|B[1-3]|Caa|Ca|C|P-[1-3]|NP)$", ErrorMessage = "Le format de la note donnée par Moody’s n'est pas valide")]
         public string MoodysRating { get; set; }
+
+
+        [Required(ErrorMessage = "La note donnée par Standard & Poor’s doit être renseignée")]
+        [MinLength(1, ErrorMessage = "La note donnée par Standard & Poor’s doit contenir au moins 1 caractère")]
+        [MaxLength(4, ErrorMessage = "La note donnée par Standard & Poor’s ne doit pas dépasser 4 caractères")]
+        [RegularExpression(@"^(AAA|AA[+-]?|A[+-]?|BBB[+-]?|BB[+-]?|B[+-]?|CCC[+-]?|CC|C|D|SD)$", ErrorMessage = "Le format de la note donnée par Standard & Poor’s n'est pas valide")]
         public string SandPRating { get; set; }
+
+
+        [Required(ErrorMessage = "La note donnée par Fitch doit être renseignée")]
+        [MinLength(1, ErrorMessage = "La note donnée par Fitch doit contenir au moins 1 caractère")]
+        [MaxLength(4, ErrorMessage = "La note donnée par Fitch ne doit pas dépasser 4 caractères")]
+        [RegularExpression(@"^(AAA|AA[+-]?|A[+-]?|BBB[+-]?|BB[+-]?|B[+-]?|CCC[+-]?|CC|C|D|RD)$", ErrorMessage = "Le format de la note donnée par Fitch n'est pas valide")]
         public string FitchRating { get; set; }
-        public byte? OrderNumber { get; set; }
     }
 }
