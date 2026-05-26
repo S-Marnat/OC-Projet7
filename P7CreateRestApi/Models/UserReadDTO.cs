90acfae3c1315977429a1aee9e102318f46afb4e
S-Marnat <242020513+S-Marnat@users.noreply.github.com>
2026-05-26 19:21:14 +0200

    feat: implémentation des DTO

--- P7CreateRestApi/Models/UserReadDTO.cs
+++ P7CreateRestApi/Models/UserReadDTO.cs
@@ -4,7 +4,6 @@
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-        public string Password { get; set; }
         public string Fullname { get; set; }
         public string Role { get; set; }
     }
