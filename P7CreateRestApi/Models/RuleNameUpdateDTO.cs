c958827b7c95a10b77161eebd443f2fcf9d27dfd
S-Marnat <242020513+S-Marnat@users.noreply.github.com>
2026-05-26 17:29:25 +0200

    feat: implémentation des DTO

--- P7CreateRestApi/Models/RuleNameUpdateDTO.cs
+++ P7CreateRestApi/Models/RuleNameUpdateDTO.cs
@@ -1,13 +1,21 @@
-﻿namespace P7CreateRestApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P7CreateRestApi.Models
 {
     public class RuleNameUpdateDTO
     {
-        public int Id { get; set; }
+        [Required(ErrorMessage = "Le nom de la règle doit être renseigné")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "La description de la règle doit être renseignée")]
         public string Description { get; set; }
-        public string Json { get; set; }
-        public string Template { get; set; }
-        public string SqlStr { get; set; }
-        public string SqlPart { get; set; }
+
+        public string? Json { get; set; }
+
+        public string? Template { get; set; }
+
+        public string? SqlStr { get; set; }
+
+        public string? SqlPart { get; set; }
     }
 }
